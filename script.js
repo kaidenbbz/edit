45d55c1fe1ad47465fec19b29b470f04b9541a62
kaidenbbz <comefindmee@mail.ru>
2023-12-17 21:00:48 +0300

    ura

--- script.js
+++ script.js
@@ -1,118 +1,145 @@
 const fileInput = document.querySelector(".file-input"),
-filterOptions = document.querySelectorAll(".filter button"),
-filterName = document.querySelector(".filter-info .name"),
-filterValue = document.querySelector(".filter-info .value"),
-filterSlider = document.querySelector(".slider input"),
-rotateOptions = document.querySelectorAll(".rotate button"),
-previewImg = document.querySelector(".preview-img img"),
-resetFilterBtn = document.querySelector(".reset-filter"),
-chooseImgBtn = document.querySelector(".choose-img"),
-saveImgBtn = document.querySelector(".save-img");
+  filterOptions = document.querySelectorAll(".filter button"),
+  filterName = document.querySelector(".filter-info .name"),
+  filterValue = document.querySelector(".filter-info .value"),
+  filterSlider = document.querySelector(".slider input"),
+  rotateOptions = document.querySelectorAll(".rotate button"),
+  previewImg = document.querySelector(".preview-img img"),
+  resetFilterBtn = document.querySelector(".reset-filter"),
+  chooseImgBtn = document.querySelector(".choose-img"),
+  saveImgBtn = document.querySelector(".save-img");
 
-let brightness = "100", saturation = "100", inversion = "0", grayscale = "0";
-let rotate = 0, flipHorizontal = 1, flipVertical = 1;
+let brightness = "100",
+  saturation = "100",
+  inversion = "0",
+  grayscale = "0";
+let rotate = 0,
+  flipHorizontal = 1,
+  flipVertical = 1;
 
 const loadImage = () => {
-    let file = fileInput.files[0];
-    if(!file) return;
-    previewImg.src = URL.createObjectURL(file);
-    previewImg.addEventListener("load", () => {
-        resetFilterBtn.click();
-        document.querySelector(".container").classList.remove("disable");
-    });
-}
+  let file = fileInput.files[0];
+  if (!file) return;
+  previewImg.src = URL.createObjectURL(file);
+  previewImg.addEventListener("load", () => {
+    resetFilterBtn.click();
+    document.querySelector(".container").classList.remove("disable");
+  });
+};
 
 const applyFilter = () => {
-    previewImg.style.transform = `rotate(${rotate}deg) scale(${flipHorizontal}, ${flipVertical})`;
-    previewImg.style.filter = `brightness(${brightness}%) saturate(${saturation}%) invert(${inversion}%) grayscale(${grayscale}%)`;
-}
+  previewImg.style.transform = `rotate(${rotate}deg) scale(${flipHorizontal}, ${flipVertical})`;
+  previewImg.style.filter = `brightness(${brightness}%) saturate(${saturation}%) invert(${inversion}%) grayscale(${grayscale}%)`;
+};
 
-filterOptions.forEach(option => {
-    option.addEventListener("click", () => {
-        document.querySelector(".active").classList.remove("active");
-        option.classList.add("active");
-        filterName.innerText = option.innerText;
+filterOptions.forEach((option) => {
+  option.addEventListener("click", () => {
+    document.querySelector(".active").classList.remove("active");
+    option.classList.add("active");
+    filterName.innerText = option.innerText;
 
-        if(option.id === "brightness") {
-            filterSlider.max = "200";
-            filterSlider.value = brightness;
-            filterValue.innerText = `${brightness}%`;
-        } else if(option.id === "saturation") {
-            filterSlider.max = "200";
-            filterSlider.value = saturation;
-            filterValue.innerText = `${saturation}%`
-        } else if(option.id === "inversion") {
-            filterSlider.max = "100";
-            filterSlider.value = inversion;
-            filterValue.innerText = `${inversion}%`;
-        } else {
-            filterSlider.max = "100";
-            filterSlider.value = grayscale;
-            filterValue.innerText = `${grayscale}%`;
-        }
-    });
+    if (option.id === "brightness") {
+      filterSlider.max = "200";
+      filterSlider.value = brightness;
+      filterValue.innerText = `${brightness}%`;
+    } else if (option.id === "saturation") {
+      filterSlider.max = "200";
+      filterSlider.value = saturation;
+      filterValue.innerText = `${saturation}%`;
+    } else if (option.id === "inversion") {
+      filterSlider.max = "100";
+      filterSlider.value = inversion;
+      filterValue.innerText = `${inversion}%`;
+    } else {
+      filterSlider.max = "100";
+      filterSlider.value = grayscale;
+      filterValue.innerText = `${grayscale}%`;
+    }
+  });
 });
 
 const updateFilter = () => {
-    filterValue.innerText = `${filterSlider.value}%`;
-    const selectedFilter = document.querySelector(".filter .active");
+  filterValue.innerText = `${filterSlider.value}%`;
+  const selectedFilter = document.querySelector(".filter .active");
 
-    if(selectedFilter.id === "brightness") {
-        brightness = filterSlider.value;
-    } else if(selectedFilter.id === "saturation") {
-        saturation = filterSlider.value;
-    } else if(selectedFilter.id === "inversion") {
-        inversion = filterSlider.value;
+  if (selectedFilter.id === "brightness") {
+    brightness = filterSlider.value;
+  } else if (selectedFilter.id === "saturation") {
+    saturation = filterSlider.value;
+  } else if (selectedFilter.id === "inversion") {
+    inversion = filterSlider.value;
+  } else {
+    grayscale = filterSlider.value;
+  }
+  applyFilter();
+};
+
+rotateOptions.forEach((option) => {
+  option.addEventListener("click", () => {
+    if (option.id === "left") {
+      rotate -= 90;
+    } else if (option.id === "right") {
+      rotate += 90;
+    } else if (option.id === "horizontal") {
+      flipHorizontal = flipHorizontal === 1 ? -1 : 1;
     } else {
-        grayscale = filterSlider.value;
+      flipVertical = flipVertical === 1 ? -1 : 1;
     }
     applyFilter();
-}
-
-rotateOptions.forEach(option => {
-    option.addEventListener("click", () => {
-        if(option.id === "left") {
-            rotate -= 90;
-        } else if(option.id === "right") {
-            rotate += 90;
-        } else if(option.id === "horizontal") {
-            flipHorizontal = flipHorizontal === 1 ? -1 : 1;
-        } else {
-            flipVertical = flipVertical === 1 ? -1 : 1;
-        }
-        applyFilter();
-    });
+  });
 });
 
 const resetFilter = () => {
-    brightness = "100"; saturation = "100"; inversion = "0"; grayscale = "0";
-    rotate = 0; flipHorizontal = 1; flipVertical = 1;
-    filterOptions[0].click();
-    applyFilter();
-}
+  brightness = "100";
+  saturation = "100";
+  inversion = "0";
+  grayscale = "0";
+  rotate = 0;
+  flipHorizontal = 1;
+  flipVertical = 1;
+  filterOptions[0].click();
+  applyFilter();
+};
 
 const saveImage = () => {
-    let main = document.getElementById("main-img");
-    let avatar = document.getElementById("avatar-img");
-    avatar.src = main.src;
-    const mainStyles = main.getAttribute("style");
-    avatar.setAttribute("style", mainStyles);
-}
+  let main = document.getElementById("main-img");
+  let avatar = document.getElementById("avatar-img");
+  avatar.src = main.src;
+  const mainStyles = main.getAttribute("style");
+  avatar.setAttribute("style", mainStyles);
+};
 async function testCamera() {
-    const videoElement = document.getElementById('camera-preview');
-  
-    try {
-      const stream = await navigator.mediaDevices.getUserMedia({ video: true });
-      videoElement.srcObject = stream;
-      alert('Camera is working!');
-    } catch (error) {
-      alert(`Error accessing camera: ${error.name}`);
-    }
-  }
+  const videoElement = document.getElementById("camera-preview");
 
+  try {
+    const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+    videoElement.srcObject = stream;
+    alert("Camera is working!");
+  } catch (error) {
+    alert(`Error accessing camera: ${error.name}`);
+  }
+}
+const makePicture = () => {
+  const videoElement = document.getElementById("camera-preview");
+  let main = document.getElementById("main-img");
+  const canvas = document.createElement("canvas");
+  canvas.width = videoElement.videoWidth;
+  canvas.height = videoElement.videoHeight;
+  const context = canvas.getContext("2d");
+
+  // Отрисовка текущего кадра видео на canvas
+  context.drawImage(videoElement, 0, 0, canvas.width, canvas.height);
+
+  // Получение данных изображения в формате base64
+  const imageDataUrl = canvas.toDataURL("image/png");
+
+  // Установка изображения в элемент <img>
+  main.src = imageDataUrl;
+  videoElement.style.display = "none";
+};
 
 filterSlider.addEventListener("input", updateFilter);
 resetFilterBtn.addEventListener("click", resetFilter);
 saveImgBtn.addEventListener("click", saveImage);
 fileInput.addEventListener("change", loadImage);
-chooseImgBtn.addEventListener("click", () => fileInput.click());
+
